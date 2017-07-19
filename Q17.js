a615cf0da5a7a7555ebc139c8c37973854033801
Iran Mas <imas@fvi.edu>
2017-07-18 17:46:30 -0700

    Test_2_Finished

---
+++ Q17.js
@@ -0,0 +1,8 @@
+// Create a for loop that prints all the even numbers from 1 to 20 to the console.
+// Use an if statement and the modulo operator in your solution.
+
+
+for (num = 1; num <= 20; num++){
+    if (num % 2 === 0)
+    console.log(num);
+}
