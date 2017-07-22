c565d86d331796b927b585cb140d02bef6018355
Brenton Cozby <brenton.cozby@gmail.com>
2017-07-22 12:35:05 -0400

    Grade it

--- Q6.js
+++ Q6.js
@@ -1,3 +1,6 @@
 // Log to the console a Boolean expression that
 // tests whether two numbers are not equal, and whose answer evaluates to true.
 console.log(5 >= 5);
+
+// Teacher feedback:
+// It asks you to test if they are "not equal" so you must use !==, not >=
